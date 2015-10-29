34a971f66b4c74aaa22a84a6986c4ec45b47418f
Shay Davidson <shay.h.davidson@gmail.com>
2015-10-29 17:20:58 +0200

    Made scss variables work

--- src/vue/app.vue
+++ src/vue/app.vue
@@ -3,7 +3,14 @@
 </template>
 
 <style lang="sass">
-    body {
-        background-color: blue;
+    @import "../scss/variables.scss";
+
+    body, html {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        background-color: #eee;
+        font-size: $base-font-size;
+        box-sizing: border-box;
     }
 </style>
