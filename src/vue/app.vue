fde452e0cc10cab4b364593b66c60e83ac08b875
Shay Davidson <shay.h.davidson@gmail.com>
2015-11-05 23:20:22 +0200

    Flex box

--- src/vue/app.vue
+++ src/vue/app.vue
@@ -1,5 +1,5 @@
 <template lang="jade">
-    .input-container
+    .inputs-container
         prefix-input-field(
             class="location-input"
             prefix="I'm traveling to "
@@ -9,6 +9,7 @@
 </template>
 
 <style lang="less">
+    @import (reference) "../less/variables.less";
     @import (reference) "../less/mixins.less";
 
     body, html {
@@ -16,8 +17,21 @@
         width: 100%;
         height: 100%;
         margin: 0;
+        padding: 0;
         background-color: #eee;
         box-sizing: border-box;
+    }
+
+    .inputs-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        > div {
+            width: @base-content-width;
+        }
     }
 </style>
 
--- src/vue/prefix_input_field.vue
+++ src/vue/prefix_input_field.vue
@@ -1,12 +1,12 @@
 <template lang="jade">
     .prefix-input-field-wrapper
         .prefix-input-field-prefix {{prefix}}
-        input(
-            v-el:input
-            type="text"
-            class="prefix-input-field"
-            placeholder="..."
-        )
+        span.prefix-input-field-span
+            input.prefix-input-field(
+                v-el:input
+                type="text"
+                placeholder="..."
+            )
 </template>
 
 <style lang="less">
@@ -14,10 +14,14 @@
     @import (reference) "../less/mixins.less";
 
     .prefix-input-field-wrapper {
-        background-color: gray;
+        background-color: @color-white;
         border-radius: @base-border-radius;
         padding: @base-inner-padding;
+        border: 0.125em solid @color-pink;
         box-sizing: border-box;
+        white-space: nowrap;
+        display: inline-flex;
+        width: 100%;
 
         .prefix-input-field-prefix {
             display: inline-block;
@@ -28,11 +32,16 @@
             }
         }
 
-        .prefix-input-field {
-            .base-font();
-            background-color: transparent;
-            border: none;
-            outline: none;
+        .prefix-input-field-span {
+            flex-grow: 100;
+            
+            .prefix-input-field {
+                .base-font();
+                background-color: transparent;
+                border: none;
+                outline: none;
+                width: 100%;
+            }
         }
     }
 </style>
